0cbe7ee31c3e6c783237767c196e6900302a4295
Choiyounho <maxosa78@gmail.com>
2021-10-29 13:40:09 +0900

    init project

---
+++ app/src/main/java/com/soten/kiosk/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.soten.kiosk
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.soten.kiosk.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
